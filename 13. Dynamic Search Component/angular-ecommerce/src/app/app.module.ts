21ad44079b78faaf8a91ac60ee0673a8c9f93684
yariwa <169130283+yariwa@users.noreply.github.com>
2024-06-08 21:28:16 +0300

    U

---
+++ 13. Dynamic Search Component/angular-ecommerce/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { ProductService } from './services/product.service';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+
+const routes: Routes = [
+  {path: 'category/:id/:name', component: ProductListComponent},
+  // {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ProductCategoryMenuComponent
+  ],
+  imports: [
+    RouterModule.forRoot(routes),
+    BrowserModule,
+    AppRoutingModule 
+  ],
+  providers: [provideHttpClient(), ProductService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
