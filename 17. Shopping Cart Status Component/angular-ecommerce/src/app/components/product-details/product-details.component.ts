ec4019876ece21445b3a4df2a537312116c869a9
yariwa <169130283+yariwa@users.noreply.github.com>
2024-06-12 18:12:57 +0300

    U

---
+++ 17. Shopping Cart Status Component/angular-ecommerce/src/app/components/product-details/product-details.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../common/product';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+import { CartService } from '../../services/cart.service';
+import { CartItem } from '../../common/cart-item';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrl: './product-details.component.css'
+})
+export class ProductDetailsComponent implements OnInit {
+
+  product!: Product;
+
+  constructor(private productService: ProductService,
+    private cartService: CartService,
+    private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+
+    this.route.paramMap.subscribe(() => {
+      this.handleProductDetails();
+    })
+
+
+
+  }
+  handleProductDetails() {
+    // get the "id" param string. convert string to a number using the "+" symbol
+    const theProductId: number = +this.route.snapshot.paramMap.get('id')!;
+    this.productService.getProduct(theProductId).subscribe(
+      data => {
+        this.product = data;
+      }
+    )
+  }
+
+  addToCart() {
+    console.log(`Adding to cart: ${this.product.name}, ${this.product.unitPrice}`);
+    const theCartItem = new CartItem(this.product);
+    this.cartService.addToCart(theCartItem);
+  }
+}
